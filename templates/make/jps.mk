a5e05b339aa1bb221abe6ce773e5aa3512104bd6
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2023-03-21 23:26:18 +0800

    fix jps.mk

--- templates/make/jps.mk
+++ templates/make/jps.mk
@@ -1,7 +1,9 @@
 all: ${BUILD_CLUALIB_DIR}/jps.so
 
 JPS_SOURCE=3rd/jps/jps.c \
-	3rd/jps/fibheap.c
+	3rd/jps/heap.c \
+	3rd/jps/intlist.c \
+	3rd/jps/luabinding.c
 
 ${JPS_SOURCE}:
 	git submodule update --init 3rd/jps
