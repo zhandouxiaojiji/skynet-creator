7c6088ab2a55cbfd988eb3ee587392ca7549ff4f
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2023-07-25 19:36:36 +0800

    fix luacheck

--- modules/luacheck.sh
+++ modules/luacheck.sh
@@ -2,4 +2,4 @@
 
 cp -v -n $RESOURCES_DIR/templates/luacheck/.luacheckrc ./
 cp -v $RESOURCES_DIR/templates/luacheck/pre-commit ./.git/hooks/
-chmod 755 ./.git/hooks/pre-commit
+chmod 775 ./.git/hooks/pre-commit
