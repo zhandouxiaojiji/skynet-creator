e31284cdad322abfc444c99f736bbeaf42665eb9
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2024-01-16 18:19:22 +0800

    fix quadtree.mk

--- templates/make/quadtree.mk
+++ templates/make/quadtree.mk
@@ -1,10 +1,11 @@
 all: ${BUILD_CLUALIB_DIR}/quadtree.so
 
-QUADTREE_SOURCE=3rd/quadtree/quadtree/Quadtree.c \
+QUADTREE_SOURCE=3rd/quadtree/int_list/IntList.c \
+	3rd/quadtree/quadtree/Quadtree.c \
 	3rd/quadtree/quadtree/luabinding.c
 
 ${QUADTREE_SOURCE}:
 	git submodule update --init 3rd/quadtree
 
 ${BUILD_CLUALIB_DIR}/quadtree.so: ${QUADTREE_SOURCE}
-	${CC} $(CFLAGS) $(SHARED) -I3rd/quadtree/ -Iskynet/3rd/lua $^ -o $@
+	${CC} $(CFLAGS) $(SHARED) -I3rd/quadtree/ -I3rd/quadtree/int_list -Iskynet/3rd/lua $^ -o $@
