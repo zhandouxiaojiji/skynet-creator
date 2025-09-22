d01f634de6e36ada4f4c0f628169c0c88b79e768
huangjx <zhandouxiaojiji@gmail.com>
2025-09-22 17:36:23 +0800

    Update pbc.mk

--- templates/make/pbc.mk
+++ templates/make/pbc.mk
@@ -6,9 +6,9 @@ PBC_BINDING=lualib/protobuf.lua
 ${PBC_SOURCE}:
 	git submodule update --init 3rd/pbc
 
-${PBC_BINDING}:
-	cp 3rd/pbc/binding/lua53/protobuf.lua lualib/protobuf.lua	
+${PBC_BINDING}: 3rd/pbc/binding/lua53/protobuf.lua
+	cp $< $@
 
 ${BUILD_CLUALIB_DIR}/protobuf.so: ${PBC_SOURCE} ${PBC_BINDING}
-	-cd 3rd/pbc && ${MAKE} lib
+	cd 3rd/pbc && ${MAKE} LUADIR=../../skynet/3rd/lua CFLAGS="-O2 -fPIC -Wall -I../../skynet/3rd/lua" lib
 	${CC} ${CFLAGS} ${SHARED} -I3rd/pbc -Iskynet/3rd/lua 3rd/pbc/binding/lua53/pbc-lua53.c -o $@ -L3rd/pbc/build -lpbc
